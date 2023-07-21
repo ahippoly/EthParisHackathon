d96dfcf4123e7c82ad746ae52bdf4541187adca3
Idriss Cornuau <cornuau.idriss@gmail.com>
2023-07-21 22:30:42 +0200

    chore: clean commit

--- composables/useNormalize.ts
+++
@@ -1,9 +0,0 @@
-/* remove all special caracteres and replace them to get get normlized string */
-export const useNormalize = (string: string) => {
-  return string
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .toLowerCase()
-    .split(' ')
-    .join('')
-}
