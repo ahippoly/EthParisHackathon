79947dd084992c8acc37314151bb34edae91fca4
Arnaud <arnaud.hippo@gmail.com>
2023-07-22 22:26:09 +0200

    added types for crypto js

--- Front/modules/ethers/keyEncrypter.ts
+++ Front/modules/ethers/keyEncrypter.ts
@@ -1,15 +1,12 @@
-import { AES, enc } from 'crypto-js';
-
-
+import { AES, enc } from 'crypto-js'
 
 export function encryptKey(privateKey: string, userPassword: string): string {
-  const encryptedPrivateKey = AES.encrypt(privateKey, userPassword).toString();
-  return encryptedPrivateKey;
+  const encryptedPrivateKey = AES.encrypt(privateKey, userPassword).toString()
+  return encryptedPrivateKey
 }
 
 export function decryptKey(encryptedPrivateKey: string, userPassword: string): string {
-  const decryptedBytes = AES.decrypt(encryptedPrivateKey, userPassword);
-  const decryptedPrivateKey = decryptedBytes.toString(enc.Utf8);
-  return decryptedPrivateKey;
+  const decryptedBytes = AES.decrypt(encryptedPrivateKey, userPassword)
+  const decryptedPrivateKey = decryptedBytes.toString(enc.Utf8)
+  return decryptedPrivateKey
 }
-
