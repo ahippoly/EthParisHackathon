5d4122a74170941ff8ab20b81f5d9bc89110de6e
Idriss Cornuau <cornuau.idriss@gmail.com>
2023-07-22 18:10:42 +0200

    feat: endpoints linked front to back

--- Front/composables/use-API-endpoints/users.endpoints.ts
+++ Front/composables/use-API-endpoints/users.endpoints.ts
@@ -1,36 +1,105 @@
-import { loggedUser, relevantUsers } from '@/assets/constants/mock/users.mock'
 import { User } from '@/assets/ts/classes/user'
 
+export type TRegisterUserPayload = Omit<IUser, 'id' | 'idMask' | 'profil' | 'search'> & { profileData: IUserProfile }
+export type TUpdateUserPayload = Omit<IUser, 'id' | 'idMask' | 'profil' | 'search'> & { profileData: IUserProfile }
+
 export class UsersEndpoints {
-  static async register(idMask: string): Promise<User> {
+  static readonly path = '/users'
+
+  static async register(
+    idMask: string,
+    xmtpPublicAddress: string,
+    xmtpCryptedPrivateKey: string,
+    user: TRegisterUserPayload
+  ): Promise<IRequestResult<User>> {
     // register to api
+    const { data, error } = await useRequest().post<IUser>(UsersEndpoints.path + '/register', {
+      body: {
+        ...user,
+        idMask,
+        xmtpPublicAddress,
+        xmtpCryptedPrivateKey
+      }
+    })
+
+    if (!data || error) return { data: null, error }
 
     // get new User back as pojos
-    const user = User.fromIUser(loggedUser)
-    useSessionStore().logIn(user)
+    const registedUser = User.fromIUser(data)
+    useSessionStore().logIn(registedUser)
 
-    return user
+    return { data: registedUser, error }
   }
 
-  static async logIn(): Promise<User> {
-    const user = User.fromIUser(loggedUser)
-    useSessionStore().logIn(user)
-
-    return user
+  static async logIn(idMask: string): Promise<IRequestResult<User>> {
+    return UsersEndpoints.getProfile(idMask)
   }
 
-  static async getProfile(): Promise<User> {
-    const user = User.fromIUser(loggedUser)
-    useSessionStore().logIn(user)
+  static async getProfile(idMask: string): Promise<IRequestResult<User>> {
+    const { data, error } = await useRequest().get<IUser>(UsersEndpoints.path + '/by-id-mask?id-mask=' + idMask)
 
-    return user
+    if (!data || error) return { data: null, error }
+
+    const userInstance = User.fromIUser(data)
+
+    useSessionStore().logIn(userInstance)
+
+    return { data: userInstance, error: null }
   }
 
   static async logOut(): Promise<void> {
     useSessionStore().logOut()
   }
 
-  static async getRelevantProfiles(): Promise<User[]> {
-    return relevantUsers.map((user) => User.fromIUser(user))
+  static async getUserByXmtpAddress(address: string): Promise<IRequestResult<User>> {
+    const { data, error } = await useRequest().get<IUser>(UsersEndpoints.path + '/by-address?address=' + address)
+
+    if (!data || error) return { data: null, error }
+
+    return { data: User.fromIUser(data), error: null }
+  }
+
+  static async getAllUsersByXmtpAddress(addresses: string[]): Promise<IRequestResult<User[]>> {
+    const { data, error } = await useRequest().get<IUser[]>(UsersEndpoints.path + '/all/by-address', { body: { addresses } })
+
+    if (!data || error) return { data: null, error }
+
+    return { data: data.map((user) => User.fromIUser(user)), error: null }
+  }
+
+  static async getRelevantProfiles(idMask: string): Promise<IRequestResult<User[]>> {
+    const { data, error } = await useRequest().get<IUser[]>(UsersEndpoints.path + '/relevant-matchs?id-mask=' + idMask)
+
+    if (!data || error) return { data: null, error }
+
+    const userInstances = data.map((user) => User.fromIUser(user))
+
+    return { data: userInstances, error: null }
+  }
+
+  static async updateProfile(idMask: string, payload: TUpdateUserPayload): Promise<IRequestResult<User>> {
+    const { data, error } = await useRequest().patch<IUser>(UsersEndpoints.path + '/profile?id-mask=' + idMask, { body: payload })
+
+    if (!data || error) return { data: null, error }
+
+    return { data: User.fromIUser(data), error: null }
+  }
+
+  static async updateSearch(idMask: string, payload: IUserSearch): Promise<IRequestResult<User>> {
+    const { data, error } = await useRequest().patch<IUser>(UsersEndpoints.path + '/search?id-mask=' + idMask, { body: payload })
+
+    if (!data || error) return { data: null, error }
+
+    return { data: User.fromIUser(data), error: null }
+  }
+
+  static async updateXmtpCryptedPrivateKey(idMask: string, newCryptedKey: string): Promise<IRequestResult<boolean>> {
+    const { data, error } = await useRequest().patch<boolean>(
+      UsersEndpoints.path + `/xmtp-crypted-private-key?id-mask=${idMask}&crypted-key=${newCryptedKey}`
+    )
+
+    if (data === null || error) return { data: null, error }
+
+    return { data, error: null }
   }
 }
--- Front/nuxt.config.ts
+++ Front/nuxt.config.ts
@@ -2,56 +2,54 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    '@fortawesome/fontawesome-svg-core/styles.css',
-    'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.min.css'
-  ],
+  css: ['@fortawesome/fontawesome-svg-core/styles.css', 'vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
   ssr: false,
   dev: process.env.NODE_ENV !== 'production',
   runtimeConfig: {
     // private buildtime available variables
     SOME_PRIVATE_VAR: 'server-side only',
     // public runtime available variables
-    public: {}
+    public: {
+      // api
+      BASE_API_URL: process.env.BASE_API_URL
+    }
   },
   alias: {
     '@/': './'
   },
   build: {
-    transpile: ['vuetify'],
+    transpile: ['vuetify']
   },
   vite: {
     define: {
       'process.env.DEBUG': false
     },
     css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
                   @import "@/assets/scss/variables/colors.scss";
                   @import "@/assets/scss/variables/fonts.scss";
                   @import "@/assets/scss/variables/font-size.scss";
                   @import "@/assets/scss/variables/spacing.scss";
                   @import "@/assets/scss/_css-reset.scss";
                   @import "@/assets/scss/global.scss";
-                `,
-            },
-        },
-    },
+                `
+        }
+      }
+    }
   },
   imports: {
     autoImport: true,
-    dirs: ['stores'],
+    dirs: ['stores']
   },
-  modules: [
-    ['@pinia/nuxt', { autoImports: ['defineStore'] }],
-    '@pinia-plugin-persistedstate/nuxt'
+  modules: [['@pinia/nuxt', { autoImports: ['defineStore'] }], '@pinia-plugin-persistedstate/nuxt'],
+  components: [
+    {
+      path: '~/components/',
+      pathPrefix: false
+    }
   ],
-  components: [{
-    path: '~/components/',
-    pathPrefix: false
-  }],
   app: {
     head: {
       title: 'Harpos',
@@ -60,15 +58,14 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         {
           name: 'keywords',
-          content:
-            'to-define'
+          content: 'to-define'
         },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: 'description',
           name: 'description',
           content: 'Harpos · Communication between holders'
-        },
+        }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/icon.png' },
@@ -81,4 +78,3 @@ export default defineNuxtConfig({
     }
   }
 })
-
